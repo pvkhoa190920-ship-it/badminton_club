c2f5dfb7cc2f54bbd852bb42ffdf6fdb38f1f3fa
Phan Vinh Khoa <pvkhoa190920@gmail.com>
2026-08-09 23:27:41 +0700

    chore: secure environment configuration

--- src/main/resources/static/assets/js/config.js
+++ src/main/resources/static/assets/js/config.js
@@ -4,4 +4,5 @@
 
 // Khi lập trình ở máy tính, sử dụng cổng 8080 của Spring Boot
 // BƯỚC QUAN TRỌNG: Đổi link này thành link thật khi bạn đưa Backend lên môi trường online
-const API_BASE_URL = "https://badminton-club-xbms.onrender.com";
+//const API_BASE_URL = "https://badminton-club-xbms.onrender.com";
+const API_BASE_URL = "http://localhost:8080";
